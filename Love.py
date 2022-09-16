5042d806b1399320677e5bce0e7c7e19471ddf65
Oswaldo Hernández Ávila <108857178+420Avila@users.noreply.github.com>
2022-09-16 12:05:40 -0500

    Update Love.py

--- Love.py
+++ Love.py
@@ -1,3 +1,4 @@
+
 from cProfile import label
 from gc import DEBUG_STATS
 from logging import RootLogger, root
@@ -6,35 +7,35 @@ from tkinter import *
 import random
 
 def ovbio():
-    messagebox.showinfo(message="Lo sabía", title="Te Amo")
+    messagebox.showinfo(message="I KNEW IT", title="I LOVE")
 
 def no():
-    messagebox.showinfo(message="El dispositivo ha sido bloqueado y todos los estan siendo cifrados", title="Ransomware Attack... ") 
+    messagebox.showinfo(message="The device has been locked and all files are being encrypted.", title="Ransomware Attack... ") 
 
 def button_hover(event):
-    x = random.randint(10, 400)
-    y = random.randint(10, 400)
+    x = random.randint(1, 250)
+    y = random.randint(1, 250)
     my_button_2.place(x=x, y=y)
 
 def exit_(event):
-    x = random.randint(10, 400)
-    y = random.randint(10, 400)
+    x = random.randint(1, 250)
+    y = random.randint(1, 250)
     my_button_2.place(x=x, y=y)
 
 root = Tk()
-root.geometry("600x400")
-root.iconbitmap('/love.ico')
+root.geometry("800x300")
+root.iconbitmap('C:/Users/420avila/Desktop/GitHub/pruebas tecnicas/queires ser mi novia/love.ico')
 root.configure(background= '#ff05e2')
-root.title('RESPONDEME')
+root.title('I LOVE YOU')
 
-label_0 = Label(root, text="Quieres ser mi NOVIA?", bg='#ff05e2',fg="black", width=0, font=("BubbleGum", 30))
+label_0 = Label(root, text="Do you want to be my girlfriend?", bg='#ff05e2',fg="black", width=0, font=("BubbleGum", 30))
 label_0.place(x=90, y=60)
 
-my_button_1 = Button(root, text="SI", width=5, height=1, font=("BubbleGum", 30), bg='#ff0505', fg="white", command = ovbio)
-my_button_1.place(x=100, y=220)
+my_button_1 = Button(root, text="YES", width=5, height=1, font=("BubbleGum", 30), bg='#ff0505', fg="white", command = ovbio)
+my_button_1.place(x=200, y=150)
 
 my_button_2 = Button(root, text="NO", width=5, height=1, font=("bubbleGum", 30), bg='#ff0505', fg="white", command = no)
-my_button_2.place(x=350, y=220)
+my_button_2.place(x=500, y=150)
 
 my_button_2.bind("<Enter>", button_hover)
 my_button_2.bind("<Leave>", exit_)
